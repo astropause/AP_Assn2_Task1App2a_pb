2446e94c2cafb4dd69ddf06f4547a616a2b78729
Pause Boden <eboden1@slu.edu>
2025-09-08 20:14:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, Ma
     zoom: 10 /* Zoom level */
   });
 
- const featureLayer_2 = new FeatureLayer({url: "https://services7.arcgis.com/ZodPOMBKsdAsTqF4/arcgis/rest/services/St_Louis_City_Neighborhoods/FeatureServer"});
+ const featureLayer_2 = new FeatureLayer({url: "https://services7.arcgis.com/ZodPOMBKsdAsTqF4/arcgis/rest/services/St_Louis_City_Neighborhoods/FeatureServer/0"});
                                  
-                                  const featureLayer_1 = new FeatureLayer({url: "https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/st_louis_mo_boundary/FeatureServer"});      
+                                  const featureLayer_1 = new FeatureLayer({url: "https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/st_louis_mo_boundary/FeatureServer/0"});      
                                               map.addMany([featureLayer_1, featureLayer_2]);
 });
